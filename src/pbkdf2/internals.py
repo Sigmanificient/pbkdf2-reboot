1917c2128ba0612d2600f5ad599898ea3da52de6
sigmanificient <edhyjox@gmail.com>
2022-05-09 02:14:56 +0200

    :recycle: Improve pythonic improvements

--- src/pbkdf2/internals.py
+++ src/pbkdf2/internals.py
@@ -1,92 +1,49 @@
 __version__ = "1.3"
-__all__ = ["PBKDF2", "crypt"]
+__all__ = ("PBKDF2", "crypt")
 
 import hmac
 from base64 import b64encode as _b64encode
 from binascii import b2a_hex as _b2a_hex
-from hashlib import sha1 as SHA1
+from hashlib import sha1
 from random import randint
 from struct import pack
-
+from string import ascii_letters, digits
 
 LIMIT = 0xFFFFFFFF
 EMPTY_bSTR = "".encode("latin-1")
 
-
-def b64encode(data, chars="+/"):
-    if isinstance(chars, str):
-        return _b64encode(data, chars.encode("utf-8")).decode("utf-8")
-    else:
-        return _b64encode(data, chars)
+BASE_64_ALT_CHARS = './'
+ALLOWED_CHARS = ascii_letters + digits + BASE_64_ALT_CHARS
 
 
-class PBKDF2(object):
-    """PBKDF2.py : PKCS#5 v2.0 Password-Based Key Derivation
+def b64encode(data, alt_chars=BASE_64_ALT_CHARS.encode("utf-8")):
+    return _b64encode(data, alt_chars).decode("utf-8")
+
+
+class PBKDF2:
+    """
     This implementation takes a passphrase and a salt (and optionally an
     iteration count, a digest module, and a MAC module) and provides a
     file-like object from which an arbitrarily-sized key can be read.
     If the passphrase and/or salt are unicode objects, they are encoded as
     UTF-8 before they are processed.
+
     The idea behind PBKDF2 is to derive a cryptographic key from a
     passphrase and a salt.
+
     PBKDF2 may also be used as a strong salted password hash.  The
     'crypt' function is provided for that purpose.
+
     Remember: Keys generated using PBKDF2 are only as strong as the
     passphrases they are derived from.
     """
 
     def __init__(
-        self, passphrase, salt, iterations=1000, digest_module=SHA1, mac_module=hmac
+        self, passphrase, salt, iterations=1000, digest_module=sha1, mac_module=hmac
     ):
         self.__mac_module = mac_module
         self.__digest_module = digest_module
-        self._setup(passphrase, salt, iterations, self._pseudorandom)
 
-    def _pseudorandom(self, key, msg):
-        """Pseudorandom function.  e.g. HMAC-SHA1"""
-        return self.__mac_module.new(
-            key=key, msg=msg, digestmod=self.__digest_module
-        ).digest()
-
-    def read(self, bytes_):
-        """Read the specified number of key bytes."""
-        if self.closed:
-            raise ValueError("file-like object is closed")
-
-        size = len(self.__buf)
-        blocks = [self.__buf]
-        i = self.__blockNum
-        while size < bytes_:
-            i += 1
-            if i > LIMIT or i < 1:
-                # We could return "" here, but
-                raise OverflowError("derived key too long")
-            block = self.__f(i)
-            blocks.append(block)
-            size += len(block)
-        buf = EMPTY_bSTR.join(blocks)
-        retval = buf[:bytes_]
-        self.__buf = buf[bytes_:]
-        self.__blockNum = i
-        return retval
-
-    def __f(self, i):
-        # i must fit within 32 bits
-        assert 1 <= i <= LIMIT
-        U = self.__prf(self.__passphrase, self.__salt + pack("!L", i))
-        result = U
-        for j in range(2, 1 + self.__iterations):
-            U = self.__prf(self.__passphrase, U)
-            result = bytes([x ^ y for (x, y) in zip(result, U)])
-        return result
-
-    def read_hex(self, octets):
-        """Read the specified number of octets. Return them as hexadecimal.
-        Note that len(obj.read_hex(n)) == 2*n.
-        """
-        return _b2a_hex(self.read(octets)).decode("us-ascii")
-
-    def _setup(self, passphrase, salt, iterations, prf):
         # Sanity checks:
 
         # passphrase and salt must be str or unicode (in the latter
@@ -95,6 +52,7 @@ class PBKDF2(object):
             passphrase = passphrase.encode("UTF-8")
         elif not isinstance(passphrase, bytes):
             raise TypeError("passphrase must be str or unicode")
+
         if isinstance(salt, str):
             salt = salt.encode("UTF-8")
         elif not isinstance(salt, bytes):
@@ -107,16 +65,67 @@ class PBKDF2(object):
             raise ValueError("iterations must be at least 1")
 
         # prf must be callable
-        if not callable(prf):
+        if not callable(self._pseudorandom):
             raise TypeError("prf must be callable")
 
         self.__passphrase = passphrase
         self.__salt = salt
         self.__iterations = iterations
-        self.__prf = prf
-        self.__blockNum = 0
-        self.__buf = EMPTY_bSTR
+        self.__prf = self._pseudorandom
+        self.__block_number = 0
+        self.__buffer = EMPTY_bSTR
         self.closed = False
+
+    def _pseudorandom(self, key, msg):
+        """Pseudorandom function.  e.g. HMAC-SHA1"""
+        return self.__mac_module.new(
+            key=key, msg=msg, digestmod=self.__digest_module
+        ).digest()
+
+    def read(self, bytes_):
+        """Read the specified number of key bytes."""
+        if self.closed:
+            raise ValueError("file-like object is closed")
+
+        size = len(self.__buffer)
+        blocks = [self.__buffer]
+        i = self.__block_number
+
+        while size < bytes_:
+            i += 1
+            if i > LIMIT or i < 1:
+                # We could return "" here, but
+                raise OverflowError("derived key too long")
+            block = self.__f(i)
+            blocks.append(block)
+            size += len(block)
+
+        buf = EMPTY_bSTR.join(blocks)
+        retval = buf[:bytes_]
+        self.__buffer = buf[bytes_:]
+        self.__block_number = i
+        return retval
+
+    def __f(self, i: int) -> bytes:
+        if i < 1:
+            raise ValueError("i must be at least 1")
+
+        if i > LIMIT:
+            raise OverflowError("i too large")
+
+        u = self.__prf(self.__passphrase, self.__salt + pack("!L", i))
+        result = u
+
+        for j in range(2, 1 + self.__iterations):
+            u = self.__prf(self.__passphrase, u)
+            result = bytes(x ^ y for (x, y) in zip(result, u))
+        return result
+
+    def read_hex(self, octets):
+        """Read the specified number of octets. Return them as hexadecimal.
+        Note that len(obj.read_hex(n)) == 2*n.
+        """
+        return _b2a_hex(self.read(octets)).decode("us-ascii")
 
     def close(self):
         """Close the stream."""
@@ -125,8 +134,8 @@ class PBKDF2(object):
             del self.__salt
             del self.__iterations
             del self.__prf
-            del self.__blockNum
-            del self.__buf
+            del self.__block_number
+            del self.__buffer
             self.closed = True
 
 
@@ -169,9 +178,8 @@ def crypt(word, salt=None, iterations=None):
                 raise ValueError("Invalid salt")
 
     # Make sure the salt matches the allowed character set
-    allowed = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789./"
     for ch in salt:
-        if ch not in allowed:
+        if ch not in ALLOWED_CHARS:
             raise ValueError("Illegal character %r in salt" % (ch,))
 
     if iterations is None or iterations == 400:
@@ -179,8 +187,9 @@ def crypt(word, salt=None, iterations=None):
         salt = "$p5k2$$" + salt
     else:
         salt = "$p5k2$%x$%s" % (iterations, salt)
+
     raw_hash = PBKDF2(word, salt, iterations).read(24)
-    return salt + "$" + b64encode(raw_hash, "./")
+    return salt + "$" + b64encode(raw_hash)
 
 
 # Add crypt as a static method of the PBKDF2 class
@@ -193,5 +202,5 @@ def _make_salt():
     """Return a 48-bit pseudorandom salt for crypt().
     This function is not suitable for generating cryptographic secrets.
     """
-    binary_salt = EMPTY_bSTR.join(pack("@H", randint(0, 0xFFFF)) for i in range(3))
-    return b64encode(binary_salt, "./")
+    binary_salt = EMPTY_bSTR.join(pack("@H", randint(0, 0xFFFF)) for _ in range(3))
+    return b64encode(binary_salt)
